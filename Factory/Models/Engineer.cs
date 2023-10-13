1dc2d48a152f4c1d288dd19365e39e78949795d0
onurkaymak <onurkaymak@outlook.com>
2023-10-13 08:53:18 -0700

    create Engineer class, create Engineer entity in DbContext.

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,9 @@
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    public string Experience { get; set; }
+  }
+}
