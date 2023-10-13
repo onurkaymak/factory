ccac29989a553eba92562ae0998bd9284468e2d3
onurkaymak <onurkaymak@outlook.com>
2023-10-13 16:02:34 -0700

    refactor model validation.

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -7,10 +7,15 @@ namespace Factory.Models
   {
     public int EngineerId { get; set; }
 
-    [Required(ErrorMessage = "The engineer's name can't be empty!")] // validation attribute for Name.
+    // We didn't learn this but I was able to find a solution by myself. We learnt about empty fields but not invalid values.
+    // Prompt - I should not be able to create an engineer or a machine if the form's fields are empty or contain INVALID VALUES.
+    [Required]
+    [RegularExpression("^[a-zA-Z][a-zA-Z0-9]*$", ErrorMessage = "The engineer's name can't be empty and it can only be alphabetic!")]
     public string Name { get; set; }
 
-    [Required(ErrorMessage = "Please provide an experience level! ('Beginner', 'Intermediate', 'Expert')")] // validation attribute for Experience.
+    // [Required(ErrorMessage = "Please provide an experience level! ('Beginner', 'Intermediate', 'Expert')")] // validation attribute for Experience.
+    [Required]
+    [RegularExpression("Beginner|Intermediate|Expert", ErrorMessage = "Please provide an experience level, it can be one of the three level ('Beginner', 'Intermediate', 'Expert').")]
     public string Experience { get; set; }
 
     public List<Repair> JoinEntities { get; } // collection navigation property.
