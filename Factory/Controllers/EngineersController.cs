91595d2e703fc177965bc7b930be2bb7b3ed0027
onurkaymak <onurkaymak@outlook.com>
2023-10-13 13:04:22 -0700

    refactor details route and view for join entity.

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -45,6 +45,8 @@ namespace Factory.Controllers
     public ActionResult Details(int id)
     {
       Engineer thisEngineer = _db.Engineers
+          .Include(engineer => engineer.JoinEntities)
+          .ThenInclude(join => join.Machine)
           .FirstOrDefault(engineer => engineer.EngineerId == id);
       return View(thisEngineer);
     }
