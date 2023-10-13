addda07ae43ccaec3c6f685b04ff2fbc4bb7c5e6
onurkaymak <onurkaymak@outlook.com>
2023-10-13 09:34:29 -0700

    create Engineers Controller, add Index route.

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.Tolist();
+      return View(model);
+    }
+  }
+}
