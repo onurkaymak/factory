7fa8c1da5c0f70f8d5a909ba6f2d6175bd534663
onurkaymak <onurkaymak@outlook.com>
2023-10-13 11:25:34 -0700

    add details route & view in Machines Controller.

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -42,5 +42,12 @@ namespace Factory.Controllers
         return RedirectToAction("Index");
       }
     }
+
+    public ActionResult Details(int id)
+    {
+      Machine thisMachine = _db.Machines
+          .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
   }
 }
