a91be1fc733df2b78aec2b0a9035ec7892528906
onurkaymak <onurkaymak@outlook.com>
2023-10-13 14:36:19 -0700

    refactor Details route in Machines Controller.

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -46,6 +46,8 @@ namespace Factory.Controllers
     public ActionResult Details(int id)
     {
       Machine thisMachine = _db.Machines
+          .Include(machine => machine.JoinEntities)
+          .ThenInclude(join => join.Engineer)
           .FirstOrDefault(machine => machine.MachineId == id);
       return View(thisMachine);
     }
