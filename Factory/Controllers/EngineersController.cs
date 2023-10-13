467dc838d8884dd8bbdb83ac497c585f90ce9ef1
onurkaymak <onurkaymak@outlook.com>
2023-10-13 09:58:51 -0700

    add validation for Engineer entity

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -30,9 +30,16 @@ namespace Factory.Controllers
     [HttpPost]
     public ActionResult Create(Engineer engineer)
     {
-      _db.Engineers.Add(engineer);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+      if (!ModelState.IsValid)
+      {
+        return View();
+      }
+      else
+      {
+        _db.Engineers.Add(engineer);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
     }
   }
 }
--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
+
+    [Required(ErrorMessage = "The engineer's name can't be empty!")] // validation attribute for Name.
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "Please provide an experience level! ('Beginner', 'Intermediate', 'Expert')")] // validation attribute for Experience.
     public string Experience { get; set; }
   }
 }
