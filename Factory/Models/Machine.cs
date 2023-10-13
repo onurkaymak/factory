6e158d297ec7a6e1460b7c7a15b4e1d79ee7f98d
onurkaymak <onurkaymak@outlook.com>
2023-10-13 11:12:49 -0700

    add validation attributes in Machine entity.

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -3,7 +3,11 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
+
+    [Required(ErrorMessage = "The machine's name can't be empty!")] // validation attribute for Name.
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "Please provide an difficulty level! ('Easy', 'Medium', 'Hard')")] // validation attribute for RepairDifficulty.
     public string RepairDifficulty { get; set; }
   }
 }
